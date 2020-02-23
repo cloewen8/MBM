9e5a7a3a10557480590a835d9cfd4d616c3ea673
Colton Loewen <loewencjay@outlook.com>
2020-02-22 21:19:25 -0700

    Added logger
    That was easy.
    
    ... Also small bug fix.

--- BaseMod.java
+++ BaseMod.java
@@ -1,5 +1,11 @@
-package ca.cjloewen.basemod;
+package ca.cjloewen.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BaseMod {
-	
+	/**
+	 * The main logger.
+	 */
+	protected static final Logger LOGGER = LogManager.getLogger();
 }
