37124dfb4c4685892c0dc2e6dac01705dbcd3074
Colton Loewen <loewencjay@outlook.com>
2020-02-24 03:42:12 -0700

    Bug fix

--- BaseRegistry.java
+++ BaseRegistry.java
@@ -10,7 +10,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
-public class BaseRegistry<T extends IForgeRegistryEntry<T>> extends DeferredRegister<T> {
+public abstract class BaseRegistry<T extends IForgeRegistryEntry<T>> extends DeferredRegister<T> {
 	private IForgeRegistry<T> type;
 	private String modid;
 	
