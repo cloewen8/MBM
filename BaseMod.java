cdc6a55ef9f058bdeb6cc090a96b0f52974e14e8
Colton Loewen <loewencjay@outlook.com>
2020-03-06 23:38:29 -0700

    Removed unused register

--- BaseMod.java
+++ BaseMod.java
@@ -3,8 +3,6 @@ package ca.cjloewen.base;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraftforge.common.MinecraftForge;
-
 /**
  * Provides a starting point for mods.
  */
@@ -13,9 +11,4 @@ public class BaseMod {
 	 * The main logger.
 	 */
 	protected static final Logger LOGGER = LogManager.getLogger();
-	
-	public BaseMod() {
-		// Register for server events.
-		MinecraftForge.EVENT_BUS.register(this);
-	}
 }
